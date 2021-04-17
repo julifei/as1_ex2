2dc9336525c39bee5e0d0598dbba8c69435d08ee
Julius <julius.feigl@googlemail.com>
2021-04-17 22:31:50 +0200

    Commit: version with search for all files '**'

--- ex2.py
+++ ex2.py
@@ -6,20 +6,47 @@ Exercise 2
 
 import os
 import glob
-from PIL import image
+from PIL import Image
 import tqdm
 import numpy as np
 
-#enter input direction
-inp_dir = r"/home/osboxes/Documents/Python II/as1/Ex_1/data_pictures_small"
+# enter input direction
+inp_dir = r"C:\Users\Julius\Desktop\Phyton II\Unit04\04_supplements\04_supplements\04_dataset_preview\dataset_preview"
 
-#enter output direction
+# enter output direction
 out_dir = inp_dir + "_out"
 
-#enter logfile direction
+# enter logfile direction
 logfile = inp_dir + "_log"
 
 def ex2(inp_dir, out_dir, logfile):
-    image_files = sorted(glob.glob(os.path.abspath.join(inp_dir, '**',)))
-    os.makedirs(out_dir)
-    print('a')
+    correct_img = 0
+
+    # Get list of all files in input directory
+    image_files = sorted(glob.glob(os.path.join(inp_dir, '**'),
+                                   recursive=True))
+    # Check number of found files
+    print(f"{len(image_files)} files found")
+
+    # Make output directory
+    try:
+        os.makedirs(out_dir)
+    except OSError:
+        pass
+
+    # define array for hash values of the images
+    hash_list = []
+
+    # loop checks all the conditions for every single file
+    for i, image_files in tqdm(enumerate(image_files), desc="Processing files",
+                              total=len(image_files)):
+        if image_files[i].endswith('*.jpg'| '*.JPG' | '*.JPEG' | '*.jpeg'):
+            print(f'file {image_files[i]} is an image')
+
+        else:
+            with open(logfile + r'/logfile.txt', 'a') as log:
+                print(f'{image_files[i]}; 1', file=log)
+
+    return correct_img
+
+ex2(inp_dir, out_dir, logfile)
